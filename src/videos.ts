fdc90c0f51a1959a2c2a923770fa9c61af3c1f9b
kamal9580 <nileshprince3118@gmail.com>
2024-07-28 14:24:35 +0530

    completed and first commit

---
+++ src/videos.ts
@@ -0,0 +1,80 @@
+export interface Video {
+    title: string;
+    thumbnail: string;
+    description: string;
+    viewCount: string;
+    timestamp: string;
+}
+
+export const VIDEOS: Video[] = [
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    },
+    {
+        title: "New song",
+        thumbnail: "/fullstack.png",
+        description: "lets learn full stack developement.",
+        viewCount: "1 million",
+        timestamp: "1 month"
+    }
+]
